baa30940cca5b3f4a207b429271f68a1e4d6efe8
enok3n <enok3n.direct@gmail.com>
2021-04-04 08:55:18 +0300

    decimal_misc export all flag removed

--- src/decimal_misc.erl
+++ src/decimal_misc.erl
@@ -1,6 +1,6 @@
 -module(decimal_misc).
 
--compile(export_all).
+-export([is_finite/1, is_infinite/1, is_NaN/1, is_qNaN/1, is_signed/1, is_sNaN/1, is_zero/1]).
 
 is_finite(X) ->
   not is_infinite(X) andalso not is_NaN(X).
