a571a42fce348303ffcac30eb0b6ceb193979a65
enok3n <enok3n.direct@gmail.com>
2021-04-04 09:10:31 +0300

    readability fix

--- src/decimal_misc.erl
+++ src/decimal_misc.erl
@@ -1,6 +1,14 @@
 -module(decimal_misc).
 
--export([is_finite/1, is_infinite/1, is_NaN/1, is_qNaN/1, is_signed/1, is_sNaN/1, is_zero/1]).
+-export([
+  is_finite/1, 
+  is_infinite/1, 
+  is_NaN/1, 
+  is_qNaN/1, 
+  is_signed/1, 
+  is_sNaN/1, 
+  is_zero/1
+  ]).
 
 is_finite(X) ->
   not is_infinite(X) andalso not is_NaN(X).
